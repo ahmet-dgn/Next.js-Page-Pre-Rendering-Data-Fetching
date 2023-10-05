e4603b8dbdf6f00c8d1e86d2dc2ff8d2e2383af3
Ahmet Can Doğan <ahmetcandgn@gmail.com>
2023-10-05 13:43:45 +0300

    Practice getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -19,11 +19,26 @@ export async function getStaticProps() {
   //The props down below represent the props in HomePade component.
   //First, the async function executes and prepares props for HomePage component.
   //The code inside this function never renders on the client side. That's why it is safe to use for credentials, validations, etc
+  if (!data) {
+    //if there is no data redirect no-data page.
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+  if (data.products.length === 0) {
+    return { notFound: ture }; //if there are no products return 404 page.
+  }
   return {
     props: {
       products: data.products,
     },
     revalidate: 10,
+    //it rebuilds page in every 10 seconds.have to use it carefully because too much revalidate puts a load on the server.
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
   };
 }
 export default HomePage;
