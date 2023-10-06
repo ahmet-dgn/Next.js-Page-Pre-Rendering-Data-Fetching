411d3b47b562eb542f03c14616fb00d5869aea42
Ahmet Can Doğan <ahmetcandgn@gmail.com>
2023-10-06 10:00:07 +0300

    SWR

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,31 +1,53 @@
 import { useEffect, useState } from "react";
-
+import useSWR from "swr";
 function LastSalesPage() {
   const [sales, setSales] = useState();
-  const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(
+    "https://nextjs-course-39eca-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
+
   useEffect(() => {
-    setIsLoading(true);
-    fetch("https://nextjs-course-39eca-default-rtdb.firebaseio.com/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
-  if (isLoading) {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("https://nextjs-course-39eca-default-rtdb.firebaseio.com/sales.json")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         const transformedSales = [];
+  //         for (const key in data) {
+  //           transformedSales.push({
+  //             id: key,
+  //             username: data[key].username,
+  //             volume: data[key].volume,
+  //           });
+  //         }
+  //         setSales(transformedSales);
+  //         setIsLoading(false);
+  //       });
+  //   }, []);
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data || !sales) {
     return <p>Loading...</p>;
   }
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
+
   return (
     <lu>
       {sales.map((sale) => (
